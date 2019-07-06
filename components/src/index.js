3b40f2a3a738b6ec5f010e7ebabb664450497147
Frank Edward Daza González <frank.daza2@gmail.com>
2019-07-06 18:15:58 -0500

    Implementing an Approval Card.

--- components/src/index.js
+++ components/src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
 import faker from 'faker';
+import ApprovalCard from './ApprovalCard';
 
 
 const App = () => {
   return (
     <div className="ui container comments">
+
+      <ApprovalCard />
+
       <CommentDetail 
         author="Frank" 
         timeAgo="Today at 4:05PM" 
