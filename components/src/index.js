d258883a69cc81c6760c268e406dc9c394f67c33
Frank Edward Daza González <frank.daza2@gmail.com>
2019-07-06 18:02:13 -0500

    Passing Props - Solutions.

--- components/src/index.js
+++ components/src/index.js
@@ -7,11 +7,23 @@ import faker from 'faker';
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Frank" timeAgo="Today at 4:05PM" />
+      <CommentDetail 
+        author="Frank" 
+        timeAgo="Today at 4:05PM" 
+        content="Nice blog post!" 
+        avatar={faker.image.avatar()} />
 
-      <CommentDetail author="Edward" timeAgo="Today at 1:29PM" />
+      <CommentDetail 
+        author="Edward" 
+        timeAgo="Today at 1:29PM" 
+        content="I like the subject!" 
+        avatar={faker.image.avatar()} />
 
-      <CommentDetail author="Vanessa" timeAgo="Today at 12:00PM" />
+      <CommentDetail 
+        author="Vanessa" 
+        timeAgo="Today at 12:00PM" 
+        content="I like the writing!" 
+        avatar={faker.image.avatar()} />
     </div>
   );
 };
