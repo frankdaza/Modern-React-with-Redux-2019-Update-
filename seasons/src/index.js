70b65ffb5b0830c1bd18cb4f533b5198d3097a76
Frank Edward Daza González <frank.daza2@gmail.com>
2019-07-07 16:37:18 -0500

    Getting a Users Physical Location.

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 
 
 const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),
+    (error) => console.log(error)
+  );
+
   return <div>Hi there!</div>;
 };
 
