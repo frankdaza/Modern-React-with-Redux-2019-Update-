b241c254eadba6d65f6b8a43f615f6579b857eca
Frank Edward Daza González <frank.daza2@gmail.com>
2019-07-07 16:45:19 -0500

    Handling Async Operations with Functional Components.

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -8,7 +8,7 @@ const App = () => {
     (error) => console.log(error)
   );
 
-  return <div>Hi there!</div>;
+  return <div>Latitude: </div>;
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
