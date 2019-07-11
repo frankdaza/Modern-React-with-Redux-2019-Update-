9077fe1ce598f246d5f67ed7e72466c05817c6b7
Frank Edward Daza González <frank.daza2@gmail.com>
2019-07-10 19:07:06 -0500

    Introducing Lifecycle Methods.

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -22,6 +22,14 @@ class App extends React.Component {
     );
   }
   
+  componentDidMount() {
+    console.log("My component was rendered to the screen");
+  }
+  
+  componentDidUpdate() {
+    console.log("My component was just updated - it rerendered!");
+  }
+  
 
   render() {
     if (this.state.errorMessage && !this.state.lat) {
