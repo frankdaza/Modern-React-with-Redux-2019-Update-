eb7097ff6ba74b3e92f7b50729bcc73862a32910
Frank Edward Daza González <frank.daza2@gmail.com>
2019-07-14 21:14:26 -0500

    Adding Some Styling.

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,10 +1,11 @@
+import './SeasonDisplay.css';
 import React from 'react'
 
 const seasonConfig = {
-  summer: { 
+  summer: {
     text: 'Let\'s hit the beach!',
     iconName: 'sun'
-   },
+  },
   winter: {
     text: 'Burr it is cold!',
     iconName: 'snowflake'
@@ -25,11 +26,12 @@ const SeasonDisplay = props => {
   const { text, iconName } = seasonConfig[season];
 
   return (
-    <div>
-      <i className={ `${iconName} icon` }></i>
-      <h1>{ text }</h1>
-      <i className={ `${iconName} icon` }></i>
-    </div>);
+    <div className={ `season-display ${season}` }>
+      <i className={`icon-left massive ${iconName} icon`}></i>
+      <h1>{text}</h1>
+      <i className={`icon-right massive ${iconName} icon`}></i>
+    </div>
+  );
 };
 
 export default SeasonDisplay;
