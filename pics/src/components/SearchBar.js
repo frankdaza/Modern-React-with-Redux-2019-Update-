64e82e596af765e8ab2caa0e274557048e0bd121
Frank Edward Daza González <frank.daza2@gmail.com>
2019-07-17 22:30:59 -0500

    More on Controlled Elements.

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -3,9 +3,7 @@ import React, { Component } from 'react'
 
 class SearchBar extends React.Component {
 
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
+  state = { term: '' };
 
   render() {
     return (
@@ -14,7 +12,10 @@ class SearchBar extends React.Component {
 
           <div className="field">
             <label>Image Search</label>
-            <input type="text" onChange={ this.onInputChange } />
+            <input 
+              type="text" 
+              value={ this.state.term }
+              onChange={ e => this.setState({ term: e.target.value }) } />
           </div>
 
         </form>
