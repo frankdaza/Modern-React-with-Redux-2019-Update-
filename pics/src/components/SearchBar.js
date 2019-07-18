87e0f2ef2d7fa13422d8b4c99b32fc82daa92228
Frank Edward Daza González <frank.daza2@gmail.com>
2019-07-17 21:02:26 -0500

    Showing Forms to the User.

---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+
+class SearchBar extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <form>
+          <input type="text" />
+        </form>
+      </div>
+    );
+  }
+
+}
+
+export default SearchBar;
