ebc80d46fbe9f9b5f3eeb3a5c54a58cad15b62e9
Frank Edward Daza González <frank.daza2@gmail.com>
2019-07-18 22:07:33 -0500

    Binding Callbacks.

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import axios from 'axios';
 import SearchBar from './SearchBar';
+import { async } from 'q';
 
 
 class App extends React.Component {
 
   state = { images: [] };
 
-  async onSearchSubmit(term) {
+  onSearchSubmit = async (term) => {
     const response = await axios.get('https://api.unsplash.com/search/photos/', {
       params: { query: term },
       headers: {
