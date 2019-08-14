00106fece1795e54a718e9ec093c11f83daf1b90
Frank Edward Daza González <frank.daza2@gmail.com>
2019-08-13 20:37:40 -0500

    Rendering Lists.

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
 
 
 class App extends React.Component {
@@ -15,23 +16,12 @@ class App extends React.Component {
     this.setState({ images: response.data.results });
   }
 
-  // onSearchSubmit(term) {
-  //   axios.get('https://api.unsplash.com/search/photos/', {
-  //     params: { query: term },
-  //     headers: {
-  //       Authorization: 'Client-ID 0da318d43b02cc0d6a9d338e48134863e2cd8937c9cec229b5d58686ea65275a'
-  //     }
-  //   }).then(response => {
-  //     console.log(response.data.results);
-  //   });
-  // }
-
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={ this.onSearchSubmit } />
 
-        Found: { this.state.images.length } images.
+        <ImageList images={ this.state.images } />
       </div>
     );
   }
